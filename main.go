a0f62f64d94a38044e1bb5f3ab3dec10968700a6
Roman Rozin <aka.rozin@gmail.com>
2020-05-19 11:18:20 +0300

    working

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"path/filepath"
+
+	"log"
+	"os"
+	"time"
+)
+
+var version string = "hatul"
+
+func main() {
+
+	bufferSize := flag.Int("buffer", 1024, "write buffer size ")
+	fileName := flag.String("file", "large.file", "file name")
+	size := flag.Int("size", 100, "size in MB")
+
+	flag.Parse()
+
+	log.Printf("this is version %#v", version)
+
+	buf := make([]byte, *bufferSize)
+	rand.Read(buf)
+
+	perent := filepath.Dir(*fileName)
+	err := os.MkdirAll(perent, os.ModePerm)
+
+	if err != nil {
+
+		log.Printf("error creating dirs for file %#v %s", perent, err.Error())
+	}
+	file, err := os.Create(*fileName)
+	if err != nil {
+
+		log.Printf("fatal error opening file %s", err.Error())
+		return
+	}
+	defer file.Close()
+
+	start := time.Now()
+
+	for i := 0; i < 1024**size; i++ {
+		file.Write(buf)
+
+	}
+	log.Printf("took %s to create %dMB ", time.Since(start), *size)
+
+}
